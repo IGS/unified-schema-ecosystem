edc7fba9a56bcf57d15e829acdc41e7aa98b9110
abradyIGS <abrady@som.umaryland.edu>
2020-03-30 13:26:41 -0400

    Flipped import direction; rearranged subdirs; added subtype support; updated validator script and examples

--- test.py
+++ test.py
@@ -52,15 +52,15 @@ doc1 = {
       },
       "study_abbrev": "HHS"
    },
-   "dummy_HMP_property": "dummy_HMP_value"
+   "dummy_HMP_property": "dummy_value"
 }
 
 doc2 = {
    "id" : "2",
    "collection" : "NeMO",
-   "basename": "myFile_2.fastq",
-   "size_in_bytes" : 2048,
-   "dummy_NeMO_property": "dummy_NeMO_value"
+   "some_essential_mouse_property": "this_is_required_by_core_subtype_mouse_subject_schema",
+   "some_inessential_mouse_property": 42,
+   "essential_NeMO_mouse_property": "this_is_required_by_NeMO_mouse_subject_schema"
 }
 
 doc3 = {
@@ -71,12 +71,13 @@ doc3 = {
    "dummy_NeMO_property": "dummy_NeMO_value"
 }
 
-abs_schema_path = os.getcwd() + "/schemas/core/file.json"
+print(os.getcwd())
+
+# Doc 1: 
+abs_schema_path = os.getcwd() + "/dataset_specific_extension_schemas/HMP/base_types/file_HMP.json"
 
 with open(abs_schema_path, 'r') as fp:
    schema = json.loads(fp.read())
-
-print(os.getcwd())
 
 resolver = RefResolver(
    # Comment from web:
@@ -87,29 +88,51 @@ resolver = RefResolver(
    referrer=None
 )
 
-#Draft7Validator.check_schema(schema) # Comment from web: 'Unnecessary but a good idea'
+Draft7Validator.check_schema(schema) # Comment from web: 'Unnecessary but a good idea'
 
 validator = Draft7Validator(schema, resolver=resolver, format_checker=None)
 
 try:
-    print("Validating doc1")
+    print("Validating doc1 with " + abs_schema_path)
     validator.validate(doc1)
     print("Valid")
 except Exception as e:
     print(e)
 
+# Doc 2:
+abs_schema_path = os.getcwd() + "/dataset_specific_extension_schemas/NeMO/subtypes/mouse_subject_NeMO.json"
+
+with open(abs_schema_path, 'r') as fp:
+   schema = json.loads(fp.read())
+
+resolver = RefResolver(
+   # Comment from web:
+   # The key part is here where we build a custom RefResolver 
+   # and tell it where *this* schema lives in the filesystem
+   # Note that `file:` is for unix systems
+   base_uri='file:{}'.format(abs_schema_path),
+   referrer=None
+)
+
+Draft7Validator.check_schema(schema) # Comment from web: 'Unnecessary but a good idea'
+
+validator = Draft7Validator(schema, resolver=resolver, format_checker=None)
+
 try:
-    print("Validating doc2")
+    print("Validating doc2 with " + abs_schema_path)
     validator.validate(doc2)
     print("Valid")
 except Exception as e:
     print(e)
 
-try:
-    print("Validating doc3")
-    validator.validate(doc3)
-    print("Valid")
-except Exception as e:
-    print(e)
+# Doc 3:
+
+
+#try:
+#    print("Validating doc3")
+#    validator.validate(doc3)
+#    print("Valid")
+#except Exception as e:
+#    print(e)
 
 
